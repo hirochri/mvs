2b99a9666b5d305c664e227ac744d93a1c21e385
hirochri <hirochri@umich.edu>
2018-05-31 11:56:22 -0400

    Nginx now serving Flask app

--- app/app.py
+++ app/app.py
@@ -3,21 +3,21 @@ from pymongo import MongoClient
 
 
 # Connect to MongoDB, just to show we can
-'''
 users = MongoClient("mongo-database", 27017).demo.users
 users.insert_one({"username": "lightscalar"})
 users.insert_one({"username": "hirochri"})
-'''
 
 # Define server app.
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-  return 'Hello World from Flask'
+  print(users.count)
+  return 'Hello World from App, we have users: ' + str(users.count())
 
+@app.route("/test")
+def test():
+  return 'Testing another route'
 
 if __name__ == "__main__":
-    print("RUNNING NOW.")
     app.run(debug=True, host="0.0.0.0", port=3000)
