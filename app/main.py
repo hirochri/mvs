a1fb59cdfbc433fa59732346eb8659680b3c590c
hirochri <hirochri@umich.edu>
2018-05-30 20:50:27 -0400

    Initial start from minimal-docker-compose with nginx included

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from pymongo import MongoClient
+
+
+# Connect to MongoDB, just to show we can
+'''
+users = MongoClient("mongo-database", 27017).demo.users
+users.insert_one({"username": "lightscalar"})
+users.insert_one({"username": "hirochri"})
+'''
+
+# Define server app.
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return 'Hello World from Flask'
+
+
+if __name__ == "__main__":
+    print("RUNNING NOW.")
+    app.run(debug=True, host="0.0.0.0", port=3000)
