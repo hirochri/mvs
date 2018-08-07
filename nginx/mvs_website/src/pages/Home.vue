3679d0e1d0644a64e102ae5ff19374fd25504b30
hirochri <hirochri@umich.edu>
2018-08-07 10:59:59 -0400

    New homepage

--- nginx/mvs_website/src/pages/Home.vue
+++ nginx/mvs_website/src/pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
 <div id="home-page">
+  <img src='../assets/mvs_logo_white.png'/>
+
 </div>
 </template>
 
@@ -16,9 +18,17 @@ export default {
 #home-page {
   height: 100%;
   width: 100%;
-  background-image: url("../assets/stem_cell.jpg");
+  background-image: url("../assets/stem_cell2.jpg");
   background-position: center;
   background-size: cover;
+}
+
+#home-page img {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
 }
 </style>
 
